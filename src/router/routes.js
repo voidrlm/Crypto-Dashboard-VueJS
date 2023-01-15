dd666620222eb1f40d9922ce07e692dfb1664b66
voidrlm <varjun609@gmail.com>
2023-01-15 18:58:29 +0530

    Removed unused components and views

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,5 @@ const routes = [
     name: "Dashboard",
     component: () => import("../views/dashboard.vue"),
   },
-  {
-    path: "/calendar",
-    name: "Calendar",
-    component: () => import("../views/calendar.vue"),
-  },
 ];
 export default routes;
